f602e0f68a7fcd02dca9347fac29858a4754a73e
adesantos <adelasantos07@gmail.com>
2020-09-24 01:54:32 +0000

    excuse

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,26 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var ar_who = ["The dog", "My grandma", "His turtle", "My bird"];
+  var ar_action = ["ate", "peed", "crushed", "broke"];
+  var ar_what = ["my homework", "the keys", "the car"];
+  let ar_when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var excuse = document.querySelector("#excuse");
+  var who;
+  var action;
+  var what;
+  var when;
+
+  who = ar_who[Math.floor(Math.random() * ar_who.length)];
+  what = ar_what[Math.floor(Math.random() * ar_what.length)];
+  when = ar_when[Math.floor(Math.random() * ar_when.length)];
+  action = ar_action[Math.floor(Math.random() * ar_action.length)];
+
+  excuse.innerHTML = who + " " + action + " " + what + " " + when;
 };
